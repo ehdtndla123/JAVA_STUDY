931740cda81671893783bad3e10c99fb0b7ec88c
장동수 <ehdtndla123@naver.com>
2022-06-27 04:20:20 +0900

    chapter3 연산자

---
+++ chapter03/src/chapter03/chapter03.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class chapter03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 0.1==0.1f >>> false
+		부동소수점 타입은 0.1을 정확히 표현할 수가 없어서 0.1f는 
+		0.1보다 큰 값이 되어버린다 피연산자 모두를 float 타입으로
+		강제 타입 변환 후에 비교 연산을 하든지 정수로변환해서 비교하면 된다.
+		*/
+	
+		
+	}
+
+}

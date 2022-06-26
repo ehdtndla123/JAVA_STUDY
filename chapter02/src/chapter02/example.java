13a423cfc68e531ec9f41d79ecf4e7b83c416eee
장동수 <ehdtndla123@naver.com>
2022-06-27 03:36:19 +0900

    chapter2 변수와 타입

--- chapter02/src/chapter02/example.java
+++ chapter02/src/chapter02/example.java
@@ -4,7 +4,17 @@ public class example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num1=123456780;
+		int num2=123456780;
+		
+		float num3=num2;
+		num2=(int)num3;
+		
+		int result=num1-num2;
+		System.out.println(result);
+		//float 부호(1비트) 지수(8비트) 가수(23비트)
+		// 123456780 은 23비트로 표현할수없어서 근사치로 변환
+		// double은 가능하다 int 크기보다 크기 떄문에.
 	}
 
 }

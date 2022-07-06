36e70580a90b47da8efa2afe28c0d1f89e176230
장동수 <ehdtndla123@naver.com>
2022-07-07 03:14:12 +0900

    chapter16 스트림과 병렬 처리

---
+++ chapter16/src/chapter16/IteratorVsStreamExample.java
@@ -0,0 +1,27 @@
+package chapter16;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class IteratorVsStreamExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list=Arrays.asList("홍길동","신용권","감자바");
+		
+		//Iterator 
+		Iterator<String> iterator =list.iterator();
+		while(iterator.hasNext()) {
+			String name=iterator.next();
+			System.out.println(name);
+		}
+		System.out.println();
+		
+		//Stream
+		Stream<String> stream=list.stream();
+		stream.forEach(name->System.out.println(name));
+	}
+
+}
---
+++ chapter16/src/chapter16/Student.java
@@ -0,0 +1,46 @@
+package chapter16;
+
+public class Student implements Comparable<Student>{
+	public enum Sex {MALE,FEMALE}
+	public enum City {Seoul,Pusan}
+	
+	private String name;
+	private int score;
+	private Sex sex;
+	private City city;
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+	public Student(String name,int score, Sex sex) {
+		this.name=name;
+		this.score=score;
+		this.sex=sex;
+	}
+	public Student(String name,int score, Sex sex,City city) {
+		this.name=name;
+		this.score=score;
+		this.sex=sex;
+		this.city=city;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public Sex getSex() {
+		return sex;
+	}
+	public City getCity() {
+		return city;
+	}
+	@Override
+	public int compareTo(Student o) {
+		// TODO Auto-generated method stub
+		return Integer.compare(score, o.score);
+	}
+	
+	
+}
